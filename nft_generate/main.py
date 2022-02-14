b9ce1a954cc5f7f13f50bdcc013c79b5dbe2a72a
EliasJuk <eliasp.juk@hotmail.com>
2022-02-14 15:35:57 -0300

    install extension

--- nft_generate/main.py
+++ nft_generate/main.py
@@ -1,6 +1,16 @@
+from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import time
+
+# INSTALAR EXTENÇÃO
+chrome_options = ChromeOptions()
+chrome_options.add_extension('extension.crx')
+driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
 
-driver = webdriver.Chrome()
-driver.get('https://google.com/')
+# FECHAR NAVEGADOR
+input('Press [ENTER] to close browser...')
+driver.quit()
